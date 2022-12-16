06b0a270f1a0f0d2f073ea6d0fcc7baafd6ba1d9
Irene Jurna <irene.jurna@gmail.com>
2022-12-16 12:46:23 +0100

    laatste afwerking in opmaak

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Integer[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
 
@@ -10,7 +9,7 @@ public class Main {
         String ongeldig = "ongeldige invoer";
 
         Scanner scanner = new Scanner(System.in);
-        Translator translate= new Translator(alphabetic, numeric);
+        Translator translate = new Translator(alphabetic, numeric);
 
         while (play) {
             System.out.println("Type 'x' om te stoppen \nType 'v' om te vertalen");
@@ -21,7 +20,7 @@ public class Main {
                 System.out.println("Type een cijfer in van 0 t/m 9");
                 int number = scanner.nextInt();
                 scanner.nextLine();
-                if (number < 10 ) {
+                if (number < 10) {
                     String result = Translator.translate(number);
                     System.out.println("De vertaling van " + number + " is " + result);
                 } else {
@@ -31,6 +30,5 @@ public class Main {
                 System.out.println(ongeldig);
             }
         }
-
     }
 }
